b64cb1a80fe99bba7c457b573e9166636ea59cd2
diariolunar <diariolunar47@gmail.com>
2026-08-20 23:02:07 -0300

    Corrige area segura da barra superior

--- android/src/main/java/com/diariolunar/verificacaolunar/MainActivity.java
+++ android/src/main/java/com/diariolunar/verificacaolunar/MainActivity.java
@@ -2,8 +2,13 @@ package com.diariolunar.verificacaolunar;
 
 import android.app.Activity;
 import android.graphics.Color;
+import android.graphics.Insets;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowInsets;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -16,12 +21,17 @@ public class MainActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
+    Window window = getWindow();
+    window.setStatusBarColor(Color.rgb(16, 0, 31));
+    window.setNavigationBarColor(Color.rgb(16, 0, 31));
+
     webView = new WebView(this);
     webView.setLayoutParams(new ViewGroup.LayoutParams(
       ViewGroup.LayoutParams.MATCH_PARENT,
       ViewGroup.LayoutParams.MATCH_PARENT
     ));
     webView.setBackgroundColor(Color.rgb(16, 0, 31));
+    aplicarPaddingDasBarrasDoSistema(webView);
 
     WebSettings settings = webView.getSettings();
     settings.setJavaScriptEnabled(true);
@@ -37,6 +47,25 @@ public class MainActivity extends Activity {
     } else {
       webView.restoreState(savedInstanceState);
     }
+  }
+
+  private void aplicarPaddingDasBarrasDoSistema(View view) {
+    view.setOnApplyWindowInsetsListener((target, insets) -> {
+      int top;
+      int bottom;
+
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        Insets systemBars = insets.getInsets(WindowInsets.Type.systemBars());
+        top = systemBars.top;
+        bottom = systemBars.bottom;
+      } else {
+        top = insets.getSystemWindowInsetTop();
+        bottom = insets.getSystemWindowInsetBottom();
+      }
+
+      target.setPadding(0, top, 0, bottom);
+      return insets;
+    });
   }
 
   @Override
